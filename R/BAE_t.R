db6276f6c46aca51326ba71ed5b71dc746cb89e2
zdhjeff <zdhjeffrey@gmail.com>
2024-10-10 18:01:50 -0700

    Update BAE_t.R

--- R/BAE_t.R
+++ R/BAE_t.R
@@ -8,6 +8,7 @@ library(roxygen2)
 library(fastDummies)
 library(parallel)
 
+
 #' BAET simulator: this function simulates a Bayesian adaptive enrichment trial
 #' @param nt number of treatment arms
 #' @param ns number of subgroups
@@ -27,21 +28,21 @@ library(parallel)
 #' @param prior.mean the prior mean for a multivariate normal prior
 
 BAET.sim = function(nt, ns,
-                   ss.interim = c(100,140,180),
-                   response.type = "gaussian",
-                   sig.e = NULL,
-                   mean.response = matrix(c(seq(nt*ns) ), nrow = nt, ncol = ns, byrow = F),  ## input nt treatments for one subpopulation, then nt treatments for the second population
-                   prob.subpopulation = rep (1/ns, ns),
-                   prob.trtarm = rep (1/nt, nt),
-                   maxN = 300,
-                   upper = rep (0.975, ns),
-                   lower = rep (0.1, ns),
-                   rar = F,
-                   MID = rep(0, ns),
-                   prob.MID = rep(0.5, ns),
-                   N.MCMC = 3000,
-                   prior.cov = diag(100, ns*nt),
-                   prior.mean = rep(0, ns*nt) ## notice for binary/count outcome,the prior is on log-odds/log-rate, ie, prior is put one the coef of the model
+                    ss.interim.es = list(v1, v2),
+                    response.type = "gaussian",
+                    sig.e = 10,
+                    mean.response = matrix(c(seq(nt*ns) ), nrow = nt, ncol = ns, byrow = F),  ## input nt treatments for one subpopulation, then nt treatments for the second population
+                    prob.subpopulation = rep (1/ns, ns),
+                    prob.trtarm = rep (1/nt, nt),
+                    maxN = 300,
+                    upper = rep (0.975, ns),
+                    lower = rep (0.1, ns),
+                    rar = F,
+                    MID = rep(0, ns),
+                    prob.MID = rep(0.5, ns),
+                    N.MCMC = 3000,
+                    prior.cov = diag(100, ns*nt),
+                    prior.mean = rep(0, ns*nt) ## notice for binary/count outcome,the prior is on log-odds/log-rate, ie, prior is put one the coef of the model
 ){
 
   # now begin the functions:
@@ -64,6 +65,20 @@ BAET.sim = function(nt, ns,
   subpop_ind = NULL
   trt_sub = NULL
   x_design = NULL
+
+  # Initialize counts for each category
+  counts <- rep(0, ns)
+  # Create a list to hold all vectors
+  threshold_list <- ss.interim.es
+  # Initialize threshold indices for each group (starting at the first threshold)
+  threshold_indices <- rep(1, ns)
+  # Set the initial thresholds for each group (first value in each vector)
+  current_thresholds <- sapply(threshold_list, function(v) v[1])
+  # Storage for generated data
+  generated_data <- NULL
+  # Set a maximum sample size
+  n_max <- 400
+  total_samples <- 0  # Counter for total samples generated
 
 
   check = list() # check results for each subpopualtion (ns), for the nt treatments
@@ -96,21 +111,51 @@ BAET.sim = function(nt, ns,
   upbound = list()
   sd.est = list()
   est= list()
-  ss.interim.ext = c(ss.interim, maxN)
-  nb = rep(0, length(ss.interim.ext)) ## this is the batch sample size
-  nb[1] = ss.interim.ext[1]
-  for (i in 2: (length(ss.interim.ext))) {
-    nb[i] = ss.interim.ext[i] - ss.interim.ext[i-1]
-  }
 
+  nb= c()
   j = 0
+  sg = c() # indicator of which subgroup is checked during interim looks
   #######################  Start the loop ###########################
   repeat{
     j = j + 1
+    while (any(threshold_indices <= sapply(threshold_list, length)) && total_samples < maxN) {
 
-    if (sum(nb[1:j]) > maxN) nb[j] = maxN - sum(nb[1:(j-1)])
-    x_subpop_b = t(rmultinom(nb[j], 1, prob = prob.subpop)) # this is the matrix indicating which subpopulation for the patient, in the batch
+      # Generate a single observation from the multinomial distribution
+      new_obs <- rmultinom(1, size = 1, prob = prob.subpop)
 
+      # Update counts for each category
+      counts <- counts + new_obs
+      total_samples <- total_samples + 1
+
+      # Store the generated observation
+      generated_data <- rbind(generated_data, t(new_obs))
+
+      if (any(counts >= current_thresholds)) {
+        break
+      }
+
+    }
+    # Check if any of the counts have reached their current threshold
+    for (i in 1:ns) {
+      if (counts[i] >= current_thresholds[i] && threshold_indices[i] <= length(threshold_list[[i]])) {
+        sg[j] = i
+        # Move to the next threshold in the vector for that category if it's not the last threshold
+        if (threshold_indices[i] < length(threshold_list[[i]])) {
+          threshold_indices[i] <- threshold_indices[i] + 1
+          current_thresholds[i] <- threshold_list[[i]][threshold_indices[i]]
+        } else {
+          # Set threshold to a very large number if the last threshold is reached
+          current_thresholds[i] <- Inf
+        }
+      }
+    }
+
+
+    # if (sum(nb[1:j]) > maxN) nb[j] = maxN - sum(nb[1:(j-1)])
+
+    x_subpop_b = generated_data # this is the matrix indicating which subpopulation for the patient, in the batch
+    nb[j] = nrow(x_subpop_b) # this is the batch size
+    generated_data =  NULL # reset this matrix after giving its value to x_subpop_b
     x_trtarm_b = matrix( rep(0,nb[j]*nt), nrow = nb[j], ncol=nt)
 
     for (i in 1:ns) {
@@ -285,8 +330,63 @@ BAET.sim = function(nt, ns,
 
     ##### The above sampling process is done, the following is the decision part, based on the posterior probability ####
     ### this for loop is for decisions in each subpopulation of ns
-    for (i in 1:ns){
+    if (length(y) == maxN){
+      for (i in 1:ns){
 
+        if(sum(prob_assign[[i]] == 0) >0) { # this means there is at least on treatment has a probability of assignment equals to 0
+          check[[i]][,which(prob_assign[[i]] == 0)] =0 # this indicates that once the probability of assigning subject to a treatment is 0, then the check for that trt is 0
+        }
+
+        if(sum(prob_assign[[i]]>0) ==1){ # this means only one treatment left
+          check[[i]][,which(prob_assign[[i]]>0)] = rep(1,N.MCMC)
+        }
+        else if(sum(prob_assign[[i]]>0) > 1){
+          check[[i]][,which(prob_assign[[i]]>0)] = t(apply( (thetaall[[i]])[,which(prob_assign[[i]]>0),j+1,drop=F], 1, supority_comp))# sup_check is function to check which one is largest and then return it to 1
+        }
+
+        prob_superiority[[i]] = abind(prob_superiority[[i]], apply(check[[i]], 2, mean), along = 2)
+
+        treat_prob[[i]][which(treat_result[[i]]==0)]=0 ## once the subgroup failed in mini check, it is zero
+        treat_prob[[i]][which(treat_result[[i]]==1)]= as.vector( (apply((thetaall_new_split[[i]][,which(treat_result[[i]]==1),drop=F]-MID[i] >=0 ),2,sum))/N.MCMC ) ## minioutcome_1 is the minimum outcome needed, treatp is the probablity of larger than minioutcome_1
+        treat_result[[i]]=as.numeric(treat_prob[[i]]>= prob.MID[i] ) ## whether that the treatment arm is larger than the threshold
+        prob_assign[[i]][which(treat_result[[i]]==0)]=0 # this means once the trt fails in mini check, that trt stops receiving subjects
+
+        prob_sup_minioutcome[[i]]=abind(prob_sup_minioutcome[[i]],treat_prob[[i]],along = 2) ## this is only to record treat_prob[[i]]
+        treat_result_record[[i]]= abind(treat_result_record[[i]],treat_result[[i]],along = 2)## this is only to record treat_result[[i]]
+
+        ntj[i] = length(which(apply(check[[i]], 2, mean)>lower[i] & treat_result[[i]]==1)) ## No. of groups left
+        if (ntj[i]!=0){
+          prob_assign[[i]] = rep(0, nt)
+          if (rar == T  & ntj[[i]]>1)  {
+            prob_assign[[i]][which( apply(check[[i]], 2, mean)>lower[i] & treat_result[[i]]==1)] =
+              ((ntj[i] - 1)/ntj[i])*sqrt(prob_superiority[[i]][which(apply(check[[i]], 2, mean)>lower[i]& treat_result[[i]]==1),threshold_indices[i]])
+          } else prob_assign[[i]][which( apply(check[[i]], 2, mean)>lower[i]& treat_result[[i]]==1)] = 1/ntj[i]
+
+        }
+
+        ## C1 first stopping criteria
+        if (max(apply(check[[i]], 2, mean)) >= upper[i] & prob_assign[[i]][which.max(apply(check[[i]], 2, mean))] != 0 ){ # the posterior should larger than threshold and that arm is still in
+
+          #prob_assign[[i]] = rep(0, nt)
+          prob.subpop[i]=0
+
+          C1[i]=1 ## this indicates the best subgroup is found
+
+          if (which.max(apply(check[[i]], 2, mean)) == which.max(mean.response[1:nt,i])) powerind[i]=1 # this indicates the best subgroup was found and it is truely the best
+
+        }
+
+        ## C2 second stopping criteria
+        if (sum(prob_assign[[i]]==0)==nt) { # this means all trts in the ith subpopulation are failed
+          prob_assign[[i]] = rep(0, nt)
+          prob.subpop[i]=0
+          ########
+
+          C2[i]=1 ## this indicates all subgroups failed
+        }
+      }
+    }else{
+      i = sg[j] # only make decisions for this subgroup
       if(sum(prob_assign[[i]] == 0) >0) { # this means there is at least on treatment has a probability of assignment equals to 0
         check[[i]][,which(prob_assign[[i]] == 0)] =0 # this indicates that once the probability of assigning subject to a treatment is 0, then the check for that trt is 0
       }
@@ -308,25 +408,25 @@ BAET.sim = function(nt, ns,
       prob_sup_minioutcome[[i]]=abind(prob_sup_minioutcome[[i]],treat_prob[[i]],along = 2) ## this is only to record treat_prob[[i]]
       treat_result_record[[i]]= abind(treat_result_record[[i]],treat_result[[i]],along = 2)## this is only to record treat_result[[i]]
 
-      ntj[i] = length(which(prob_superiority[[i]][,j+1]>lower[i] & treat_result[[i]]==1)) ## No. of groups left
+      ntj[i] = length(which(apply(check[[i]], 2, mean)>lower[i] & treat_result[[i]]==1)) ## No. of groups left
       if (ntj[i]!=0){
         prob_assign[[i]] = rep(0, nt)
         if (rar == T  & ntj[[i]]>1)  {
-          prob_assign[[i]][which( prob_superiority[[i]][,j+1]>lower[i] & treat_result[[i]]==1)] =
-            ((ntj[i] - 1)/ntj[i])*sqrt(prob_superiority[[i]][which(prob_superiority[[i]][,j+1]>lower[i]& treat_result[[i]]==1),j+1])
-        } else prob_assign[[i]][which( prob_superiority[[i]][,j+1]>lower[i]& treat_result[[i]]==1)] = 1/ntj[i]
+          prob_assign[[i]][which( apply(check[[i]], 2, mean)>lower[i] & treat_result[[i]]==1)] =
+            ((ntj[i] - 1)/ntj[i])*sqrt(prob_superiority[[i]][which(apply(check[[i]], 2, mean)>lower[i]& treat_result[[i]]==1),threshold_indices[i]])
+        } else prob_assign[[i]][which( apply(check[[i]], 2, mean)>lower[i]& treat_result[[i]]==1)] = 1/ntj[i]
 
       }
 
       ## C1 first stopping criteria
-      if (max(prob_superiority[[i]][,j+1]) >= upper[i] & prob_assign[[i]][which.max(prob_superiority[[i]][,j+1])] != 0 ){ # the posterior should larger than threshold and that arm is still in
+      if (max(apply(check[[i]], 2, mean)) >= upper[i] & prob_assign[[i]][which.max(apply(check[[i]], 2, mean))] != 0 ){ # the posterior should larger than threshold and that arm is still in
 
         #prob_assign[[i]] = rep(0, nt)
         prob.subpop[i]=0
-        #prob_assign[[i]][which.max(prob_superiority[[i]][,j+1])]=1 # once the bset one is picked, all non benzo patients will be assgined to this subgroup
+
         C1[i]=1 ## this indicates the best subgroup is found
 
-        if (which.max(prob_superiority[[i]][,j+1]) == which.max(mean.response[1:nt,i])) powerind[i]=1 # this indicates the best subgroup was found and it is truely the best
+        if (which.max(apply(check[[i]], 2, mean)) == which.max(mean.response[1:nt,i])) powerind[i]=1 # this indicates the best subgroup was found and it is truely the best
 
       }
 
@@ -338,6 +438,8 @@ BAET.sim = function(nt, ns,
 
         C2[i]=1 ## this indicates all subgroups failed
       }
+
+
     }
 
 
@@ -347,14 +449,14 @@ BAET.sim = function(nt, ns,
 
     if ( sum(earlystop)==ns | length(y) >= maxN) {
 
-      for (i in 1:ns) {
-        if (C1[i]==1) print(sprintf("subgroup %d stopped for having indentified the best treatment", i))
-        if (C2[i]==1) print(sprintf("subgroup %d stopped for futility of all treatments", i))
-      }
-      if (length(y) >= maxN) print("trial stopped for running out of samples")
+      # for (i in 1:ns) {
+      #   if (C1[i]==1) print(sprintf("subgroup %d stopped for having indentified the best treatment", i))
+      #   if (C2[i]==1) print(sprintf("subgroup %d stopped for futility of all treatments", i))
+      # }
+      # if (length(y) >= maxN) print("trial stopped for running out of samples")
       break
 
-      }  # this means if all subpopulation meet stop criterion , or sample size is reached, stop the trial
+    }  # this means if all subpopulation meet stop criterion , or sample size is reached, stop the trial
 
 
 
@@ -375,22 +477,25 @@ BAET.sim = function(nt, ns,
   }
 
   out = list(
-    n.interim =j,
+    n.analysis =j,
     trt_sub =trt_sub,
     est =est,
     powerind = powerind,
     y =y,
     N_terminate = length(y),
+    ss.sub=counts,
     prob_sup_minioutcome=prob_sup_minioutcome,
     #treat_result_record=treat_result_record,
-    prob_superiority=prob_superiority
+    prob_superiority=prob_superiority,
+    sg= sg
+
   )
 
   return(out)
 
 }
 
-#' @return n.interim: number of interim looks conducted to end the whole trial
+#' @return n.analysis: number of analysis conducted to end the whole trial
 #' @return trt_sub: simulated treatment arm allocation (1st column) and subgroup (2nd column)
 #' @return est: treatment effect estimation(posterior mean) and the 95% Credible interval bounds for each treatment arm in each subgroup
 #' @return powerind: power indicator of whether the best treatment arms is correctly selected in each subgroup
@@ -408,7 +513,7 @@ BAET.sim = function(nt, ns,
 Multi.BAET= function(n.sim,
                      n.cores=detectCores()/2,
                      nt=3, ns=2,
-                     ss.interim = c(100,250),
+                     ss.interim.es,
                      response.type = 'gaussian',
                      sig.e = 10,
                      mean.response = matrix( c(8,12,14, 5,8,11 ), nrow = nt, ncol = ns, byrow = F),  ## input nt treatments for one subpopulation, then nt treatments for the second population
@@ -427,7 +532,7 @@ Multi.BAET= function(n.sim,
   mt1 = foreach(i=1:n.sim, .packages = c("rjags","boot","abind","tibble", "MASS", "matlib"), .combine = "cbind") %dopar% {
 
     test= BAET.sim (nt=nt, ns=ns,
-                    ss.interim = ss.interim,
+                    ss.interim.es = ss.interim.es,
                     response.type = response.type,
                     sig.e = sig.e,
                     mean.response = mean.response,
