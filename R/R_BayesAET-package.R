a7180348813c8b959bbd1560a3db56ab9884b0ac
zdhjeff <zdhjeffrey@gmail.com>
2025-04-17 12:56:12 +0100

    Delete R_BayesAET-package.R

--- R/R_BayesAET-package.R
+++
@@ -1,5 +0,0 @@
-#' BayesAET: Bayesian Adaptive Enrichment Trial Simulation Tools
-#'
-#' This package provides tools to simulate and design Bayesian adaptive enrichment trials with pre-specified subgroups.
-#'
-"_PACKAGE"
