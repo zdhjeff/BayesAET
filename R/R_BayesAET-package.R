36abce400c2a5f10870d287c3da93edcb278cdb8
zdhjeff <zdhjeffrey@gmail.com>
2025-04-17 12:13:15 +0100

    update doc

--- R/R_BayesAET-package.R
+++ R/R_BayesAET-package.R
@@ -1,7 +1,5 @@
 #' BayesAET: Bayesian Adaptive Enrichment Trial Simulation Tools
 #'
-#' This package provides tools to simulate and design Bayesian adaptive enrichment trials with pre-defined subgroups.
+#' This package provides tools to simulate and design Bayesian adaptive enrichment trials with pre-specified subgroups.
 #'
-#' @docType package
-#' @name BayesAET
-NULL
+"_PACKAGE"
